3a2fde9d8770fbe441143588194080bd85a60010
mfarhaev <136706034+mfarhaev@users.noreply.github.com>
2023-07-19 15:29:15 +0700

    Enhance search result relevancy

---
+++ yellow_banana_flower.js
@@ -0,0 +1,438 @@
+//1
+const rideOn = () => { 
+  console.log('Ride On!'); 
+};
+
+//2
+const getStarted = () => {
+  console.log('Let's get started!');
+};
+
+//3
+const bikeBasicInfo = () => {
+  console.log('Bike is a two-wheeled, pedal-driven vehicle.'); 
+};
+
+//4
+const bikeParts = () => {
+  console.log('Parts of a bicycle include frame, wheels, saddle, chain, pedals, crankset, shifters, brakes, derailleurs, and handlebars.');
+};
+
+//5
+const bikeSafety = () => {
+  console.log('Always wear a helmet and use bicycle lights when riding at night or in poor visibility.');
+};
+
+//6
+const bikeChecklist = () => {
+  console.log('Check your bicycle tires, chain, brakes, and level and state of repair before you ride.');
+};
+
+//7
+const bikeTuneUp = () => {
+  console.log('Get a tune-up for your bicycle once a year or more often to keep it running safely and efficiently.');
+};
+
+//8
+const bikeClothing = () => {
+  console.log('Wear clothing for cycling that is comfortable and bright-colored, so you are visible to others on the road.');
+};
+
+//9
+const bikeLaneRules = () => {
+  console.log('Bicyclists must obey all traffic regulations and use bike lanes when available.');
+};
+
+//10
+const bikeSignals = () => {
+  console.log('Use hand signals to indicate turning when riding your bicycle in traffic.');
+};
+
+//11
+const easeOffBrakes = () => {
+  console.log('When biking downhill, ease off your brakes to increase speed and take advantage of gravity.');
+};
+
+//12
+const bikeOnSidewalk = () => {
+  console.log('When riding on the sidewalk, keep to the right and yield to pedestrians.');
+};
+
+//13
+const roundabouts = () => {
+  console.log('For cyclists approaching roundabouts, enter in the same lane that you plan to leave.');
+};
+
+//14
+const gears = () => { 
+  console.log('When riding in traffic, shift to a higher gear to make it easier to pedal.');
+};
+
+//15
+const bikePaths = () => {
+  console.log('When available, use bicycle paths instead of the road to make your ride safer and more enjoyable.');
+};
+
+//16
+const maintenance = () => {
+  console.log('Bicycle maintenance is an important part of keeping your bike in safe operating condition.');
+};
+
+//17
+const bikeGrips = () => {
+  console.log('Bicycle handlebar grips should be checked for wear and replaced regularly for better control.');
+};
+
+//18
+const locks = () => {
+  console.log('Always use a strong and secure bicycle lock when you leave your bike unattended.');
+};
+
+//19
+const hydration = () => {
+  console.log('Take a water bottle with you on long rides to stay hydrated.');
+};
+
+//20
+const lockToObjects = () => {
+  console.log('Lock your bike to an immovable object instead of a sign post or tree.');
+};
+
+//21
+const properlyInflated = () => {
+  console.log('Make sure your tires are properly inflated to the manufacturer\'s specifications.');
+};
+
+//22
+const brakesCheck = () => {
+  console.log('Check your brakes for proper alignment and working condition before each ride.');
+};
+
+//23
+const visible = () => {
+  console.log('Be visible by wearing bright clothing and gear and using a bright headlight or white front reflection.');
+};
+
+//24
+const adjustSaddle = () => {
+  console.log('Adjust your bicycle saddle to the height that is most comfortable and safe for you to ride.');
+};
+
+//25
+const bikeFit = () => {
+  console.log('Having a properly fitted bike is essential for a safe and comfortable ride.');
+};
+
+//26
+const wetWeather = () => {
+  console.log('In wet weather, reduce your speed and always use caution when braking.');
+};
+
+//27
+const safeDistance = () => {
+  console.log('Give vehicles at least 3 feet of clearance when passing and riding alongside them.');
+};
+
+//28
+const shadowDrivers = () => {
+  console.log('When riding in traffic, remember to shadow drivers and anticipate their movements.');
+};
+
+//29
+const bicycleChain = () => {
+  console.log('Check your bicycle chain regularly and clean and lubricate it as needed.');
+};
+
+//30
+const repairTools = () => {
+  console.log('Carry a basic repair kit and tools on long rides, in case of unexpected mechanical problems.');
+};
+
+//31
+const cellphone = () => {
+  console.log('Carry your cellphone in case of emergency, but be sure to keep it out of sight and secure.');
+};
+
+//32
+const helmets = () => {
+  console.log('Always wear a properly fitted helmet to protect your head in case of a fall or collision.');
+};
+
+//33
+const reflectors = () => {
+  console.log('Make sure your bicycle has reflectors and that they are in good condition.');
+};
+
+//34
+const secureLoads = () => {
+  console.log('Securely fasten any items you may be carrying to your bicycle, so they don\'t fall off while riding.');
+};
+
+//35
+const extraWeight = () => {
+  console.log('Reduce your speed and use extra caution when carrying extra weight on your bicycle.');
+};
+
+//36
+const downhillSpeed = () => {
+  console.log('When going downhill, control your speed to maintain a safe and comfortable riding experience.');
+};
+
+//37
+const sharingLane = () => {
+  console.log('When sharing a lane with cars, ride in the middle of the lane to make it easier for drivers to see you.');
+};
+
+//38
+const brakesMaintenance = () => {
+  console.log('Make sure you maintain your brakes and adjust them as needed, for safe and effective stopping.');
+};
+
+//39
+const cyclingPlan = () => {
+  console.log('Plan your route in advance and stick to bike-friendly roads and paths to have a safe and enjoyable ride.');
+};
+
+//40
+const adjustBike = () => {
+  console.log('Adjust the seat, handlebars, and other components of your bike to fit your body size and riding style.');
+};
+
+//41
+const bikeLaws = () => {
+  console.log('Be aware of local bicycle laws and obey them to ensure a safe and enjoyable ride.');
+};
+
+//42
+const alertOthers = () => {
+  console.log('Use hand signals to alert other riders and drivers to your intentions.');
+};
+
+//43
+const neverPassLeft = () => {
+  console.log('Never EVER pass a vehicle on the left and always give an audible warning when approaching from behind.');
+};
+
+//44
+const brakingAhead = () => {
+  console.log('Be prepared for sudden braking by riders and vehicles ahead of you.');
+};
+
+//45
+const basicTools = () => {
+  console.log('Carry basic cycling tools when you ride, such as a spare tube, tire levers, and a patch kit.');
+};
+
+//46
+const reportProblems = () => {
+  console.log('If you encounter any problems, report them to local authorities and organizations to help make roads safer for cycling.');
+};
+
+//47
+const enoughStamina = () => {
+  console.log('Make sure you have enough stamina and physical conditioning to complete your ride.');
+};
+
+//48
+const ridingSkills = () => {
+  console.log('Practice and improve your riding skills to become a confident and safe cyclist.');
+};
+
+//49
+const beVocal = () => {
+  console.log('Be vocal and assertive when riding in traffic to make sure other riders and drivers are aware of your presence.');
+};
+
+//50
+const bikeLawsKnowledge = () => {
+  console.log('Be familiar with local bicycle laws and use them to your advantage for a safe and enjoyable ride.');
+};
+
+//51
+const listenForTraffic = () => {
+  console.log('Listen for traffic and be aware of your surroundings when riding through intersections.');
+};
+
+//52
+const groupRides = () => {
+  console.log('When riding in a group, maintain a safe distance between riders and use hand signals.');
+};
+
+//53
+const alertSigns = () => {
+  console.log('Look for alert signs, such as traffic lights, stop signs, and signs warning of road hazards.');
+};
+
+//54
+const mirrorsCheck = () => {
+  console.log('Check your bicycle mirrors regularly to stay aware of your surroundings.');
+};
+
+//55
+const rideInTraffic = () => {
+  console.log('When riding in traffic, stay in the center of the lane and ride at a steady pace to avoid being passed.');
+};
+
+//56
+const nightRiding = () => {
+  console.log('When riding at night, keep a low profile and avoid riding in the dark or in areas with poor visibility.');
+};
+
+//57
+const beVisibleAtNight = () => {
+  console.log('Be visible at night by using a bright headlight and reflective clothing and equipment.');
+};
+
+//58
+const brakesCheckEveryRide = () => {
+  console.log('Check your brakes before each ride to make sure they are in good working condition for your safety.');
+};
+
+//59
+const bikeMaintenanceSchedule = () => {
+  console.log('Develop a regular bike maintenance schedule to make sure your bicycle is running well and in safe operating condition.');
+};
+
+//60
+const bikeLocksTypes = () => {
+  console.log('Choose the right type of bike lock for your bike to keep it secure when not in use.');
+};
+
+//61
+const gearShifting = () => {
+  console.log('Shift to a lower gear before approaching an uphill and to a higher gear before going downhill.');
+};
+
+//62
+const gripPosition = () => {
+  console.log('Change your grip position periodically while riding to reduce fatigue in your arms and hands.');
+};
+
+//63
+const protectiveEquipment = () => {
+  console.log('Wear protective clothing and equipment, such as gloves, knee pads, and a helmet whenever you ride.');
+};
+
+//64
+const useOfTools = () => {
+  console.log('Learn how to use basic bike tools and repair your own bike, in case of a breakdown.');
+};
+
+//65
+const routePlanning = () => {
+  console.log('Plan out your route in advance to avoid accidents and to find the safest and most efficient route.');
+};
+
+//66
+const visibilityAtIntersections = () => {
+  console.log('Be aware of your visibility at intersections and use a flashing light and/or hand signal when making a turn.');
+};
+
+//67
+const alertHumansAndAnimals = () => {
+  console.log('Alert pedestrians, animals, and other cyclists of your intentions when riding through a shared space.');
+};
+
+//68
+const understandGears = () => {
+  console.log('Understand the gears on your bike and how to use them, as they are an essential part of a safe ride.');
+};
+
+//69
+const safeRidingDistance = () => {
+  console.log('Ride at a safe distance from cars, other cyclists, and pedestrians, to avoid collisions and improve safety.');
+};
+
+//70
+const bePreparedInRain = () => {
+  console.log('Be prepared for riding in the rain by carrying proper equipment for your bike and wearing waterproof clothing.');
+};
+
+//71
+const roadPositioning = () => {
+  console.log('Pay attention to your road positioning when riding in traffic, as it can improve your visibility and safety.');
+};
+
+//72
+const obeyTrafficRules = () => {
+  console.log('Never forget that bicycle riders must obey the same traffic rules as motorists, so ride smart and safe.');
+};
+
+//73
+const forwardPlanning = () => {
+  console.log('Be mindful of your forward planning and look ahead when riding to anticipate any potential hazards.');
+};
+
+//74
+const readTheRoad = () => {
+  console.log('Read the road and anticipate the actions of other road users, to make decisions that will help keep you safe.');
+};
+
+//75
+const roadDefects = () => {
+  console.log('Watch out for road defects, such as potholes, debris, and changes in the road surface.');
+};
+
+//76
+const avoidBusyStreets = () => {
+  console.log('Whenever possible, avoid riding on busy streets or during rush hour traffic.');
+};
+
+//77
+const familiarizeDifferentRoutes = () => {
+  console.log('Familiarize yourself with different routes to reach your destination and choose the safest one.');
+};
+
+//78
+const exerciseCaution = () => {
+  console.log('Exercise caution when passing parked cars to avoid sudden opening of doors.');
+};
+
+//79
+const basicRoadRules = () => {
+  console.log('Learn and adhere to basic road rules, including keeping to the right, signaling, and stopping at stop signs.');
+};
+
+//80
+const localBicycleGroups = () => {
+  console.log('Connect with your local bicycle groups to learn more about bicycle safety and share your experiences.');
+};
+
+//81
+const checkWheelAlignment = () => {
+  console.log('Check the alignment of your wheels to make sure they are straight and true, for a smooth and safe ride.');
+};
+
+//82
+const checkBrakingDistance = () => {
+  console.log('Check the distance of your stopping when braking to make sure it is safe and effective.');
+};
+
+//83
+const checkNoise = () => {
+  console.log('Listen for any unusual noise coming from your bicycle, then take it for a tune-up if necessary.');
+};
+
+//84
+const useTrails = () => {
+  console.log('Where available, use bicycle trails for a safer and more enjoyable ride.');
+};
+
+//85
+const trainTheBasics = () => {
+  console.log('Train the basics of cycling by learning and practicing the basics of balance, braking, and speed control.');
+};
+
+//86
+const checkTirePressure = () => {
+  console.log('Check your tire pressure often to make sure your tires are properly inflated.');
+};
+
+//87
+const beAlert = () => {
+  console.log('Stay alert and anticipate the movements of other riders and vehicles on the road.');
+};
+
+//88
+const shoulderCheck = () => {
+  console.log('Perform a shoulder check before making any turns, to make sure
